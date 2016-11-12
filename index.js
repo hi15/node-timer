357fd177999614a6affdf00fb0af3e242d729b6b
Guilherme Santos <ggs12@inf.ufpr.br>
2016-11-12 19:41:52 -0200

    Testing laravel - redis - node communication

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 
 
-var app = require('express')();
-var server = require('http').Server(app);
+// var app = require('express')();
+var server = require('http').Server();
 var io = require('socket.io')(server);
+var Redis = require('ioredis');
+var redis = new Redis();
 
-server.listen(3000);
 
+redis.subscribe('test-channel');
 
-app.get('/', function(request, response){
-	response.sendFile(__dirname + '/index.html');
+redis.on('message', function(channel, message){
+    message = JSON.parse(message);
+    console.log(channel);
+    io.emit(channel + ':' + message.event, message.data); //test-channel:EventoTeste
 });
 
+
+
+
+server.listen(3000);
+//
+//
+// app.get('/', function(request, response){
+// 	response.sendFile(__dirname + '/index.html');
+// });
+//
 io.on('connection', function(socket){
 	console.log('connection made dickhead');
 
